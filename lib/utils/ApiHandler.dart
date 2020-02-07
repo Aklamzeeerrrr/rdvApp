6bd536dc4ae1d0866b0e6737fb435dd07e2a4165
Dosso Abdoul-Rhamane <abdouldosso78@gmail.com>
2020-02-07 17:36:01 +0000

    mise a jour de lib/utils/ApiHandler.dart

--- lib/utils/ApiHandler.dart
+++ lib/utils/ApiHandler.dart
@@ -1,9 +1,9 @@
 import 'package:dio/dio.dart';
 
-class Request{
+class ApiHandler{
 
 
-  void getHttp(url) async {
+  static void getHttp(url) async {
     try {
       // Response response = await Dio().get("http://www.google.com");
       Response response = await Dio().get(url);
@@ -12,4 +12,20 @@ class Request{
       print(e);
     }
   }
+
+
+  /**
+   *
+   * comment l'utiliser dans un autre fichier
+   *
+   * premierement tu importe la class
+   */
+
+/*
+    var api = ApiHandler.getHttp("https://jsonplaceholder.typicode.com/todos");
+
+    api.then(resp =>{
+      print(resp.data);
+    });
+    */
 }
